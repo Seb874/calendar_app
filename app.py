eab68477ccb9fabb22f7c59de1329070b0163ff0
Seb874 <sebastian.g.boris@gmail.com>
2025-06-25 15:49:25 -0400

    Add /responses page to view submitted availability

--- app.py
+++ app.py
@@ -20,3 +20,13 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use PORT from Render or default to 5000 locally
     app.run(debug=False, host='0.0.0.0', port=port)
 
+@app.route('/responses')
+def show_responses():
+    try:
+        with open('responses.txt', 'r') as f:
+            data = f.read().splitlines()
+    except FileNotFoundError:
+        data = []
+
+    return '<br>'.join(data)
+
